5185e2366f702a7da2c5975856efe619075e4acb
M Karthik <139629620+muneppakarthik@users.noreply.github.com>
2025-09-24 11:45:23 +0530

    myservices design

---
+++ src/components/myServices/MyServices.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./MyServices.scss";
+import SectionHeading from "../SectionHeading/SectionHeading";
+import Button from "../button/button";
+
+const MyServices = () => {
+  const data = [
+    {
+      title: "Web Development",
+      buttonLabel: "Show more",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus esse commodi deserunt vitae, vero quasi! Veniam quaerat tenetur pariatur doloribus.",
+    },
+    {
+      title: "Web Development",
+      buttonLabel: "Show more",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus esse commodi deserunt vitae, vero quasi! Veniam quaerat tenetur pariatur doloribus.",
+    },
+  ];
+  return (
+    <div className="my-services-container">
+      <SectionHeading label="My services" />
+      <div className="my-services-container__wrapper">
+        {data.map((item, index) => (
+          <div
+            className="my-services-container__wrapper__service-list-item card-shadow"
+            key={index}
+          >
+            <h5>{item.title}</h5>
+            <p>{item.description}</p>
+            <Button label={item.buttonLabel} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyServices;
