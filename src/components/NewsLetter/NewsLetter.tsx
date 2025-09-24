51124525588c222bf908738dfe2259c789fe149d
M Karthik <139629620+muneppakarthik@users.noreply.github.com>
2025-09-24 11:44:49 +0530

    news letter slider design

---
+++ src/components/NewsLetter/NewsLetter.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React from "react";
+import CustomSlider from "../CustomSlider/CustomSlider";
+import NewsLetterCard from "./NewsLetterCard";
+import "./NewsLetter.scss";
+import SectionHeading from "../SectionHeading/SectionHeading";
+
+const NewsLetter = () => {
+  const slides = [
+    {
+      image: "/images/bg.jpg",
+      date: "Sep 24, 2020",
+      dateLabel: "Events, Nature",
+      title: "The main thing for the designer – to create things.",
+      description:
+        "My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the…",
+      buttonLabel: "Read More",
+    },
+    {
+      image: "/images/bg.jpg",
+      date: "Sep 24, 2020",
+      dateLabel: "Events, Nature",
+      title: "The main thing for the designer – to create things.",
+      description:
+        "My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the…",
+      buttonLabel: "Read More",
+    },
+    {
+      image: "/images/bg.jpg",
+      date: "Sep 24, 2020",
+      dateLabel: "Events, Nature",
+      title: "The main thing for the designer – to create things.",
+      description:
+        "My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the…",
+      buttonLabel: "Read More",
+    },
+    {
+      image: "/images/bg.jpg",
+      date: "Sep 24, 2020",
+      dateLabel: "Events, Nature",
+      title: "The main thing for the designer – to create things.",
+      description:
+        "My job is simple and sophisticated, so it is possible to describe and simple, and flowery language. I love the…",
+      buttonLabel: "Read More",
+    },
+  ];
+  return (
+    <div className="newsletter-container">
+      <SectionHeading label="Newsletter" />
+      <CustomSlider
+        items={slides}
+        renderItem={(item, i) => <NewsLetterCard {...item} />}
+      />
+    </div>
+  );
+};
+
+export default NewsLetter;
---
+++ src/components/NewsLetter/NewsLetterCard.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from "../button/button";
+import Image from "next/image";
+
+const NewsLetterCard = ({
+  image = "",
+  date = "",
+  dateLabel = "",
+  title = "",
+  description = "",
+  buttonLabel = "",
+}) => {
+  return (
+    <div className="newsletter-container__card-item">
+      <div className="newsletter-container__card-item__image-block">
+        <Image
+          src={image}
+          alt=""
+          //   placeholder="blur"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+      <div className="newsletter-container__card-item__info-block">
+        <p className="newsletter-container__card-item__date">
+          {date} {dateLabel}
+        </p>
+        <h3 className="newsletter-container__card-item__title">{title}</h3>
+        <p className="newsletter-container__card-item__description">
+          {description}
+        </p>
+        <Button label={buttonLabel} />
+      </div>
+    </div>
+  );
+};
+
+export default NewsLetterCard;
