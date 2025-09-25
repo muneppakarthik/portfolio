e4ea562566aa6a8b5f4742726e5c21e03c263b88
M Karthik <139629620+muneppakarthik@users.noreply.github.com>
2025-09-25 13:18:19 +0530

    path changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import "./page.scss";
-import AboutDeveloperDetails from "@/components/AboutDeveloperDetails/AboutDeveloperDetails";
-import Banner from "@/components/Banner/Banner";
-import MyServices from "@/components/MyServices/MyServices";
-import ContactInformation from "@/components/ContactInformation/ContactInformation";
-import GetInTouch from "@/components/GetInTouch/GetInTouch";
-import NewsLetter from "@/components/NewsLetter/NewsLetter";
-import WorkEducationHistory from "@/components/WorkEducationHistory/WorkEducationHistory";
+import AboutDeveloperDetails from "../components/AboutDeveloperDetails/AboutDeveloperDetails";
+import Banner from "../components/Banner/Banner";
+import MyServices from "../components/MyServices/MyServices";
+import ContactInformation from "../components/ContactInformation/ContactInformation";
+import GetInTouch from "../components/GetInTouch/GetInTouch";
+import NewsLetter from "../components/NewsLetter/NewsLetter";
+import WorkEducationHistory from "../components/WorkEducationHistory/WorkEducationHistory";
 
 export default function Home() {
   return (
