3f9e30c730705544dd72e63abb6302c325c93adb
M Karthik <139629620+muneppakarthik@users.noreply.github.com>
2025-09-26 13:45:56 +0530

    git cache clear

--- src/components/overview/overview.tsx
+++ src/components/overview/overview.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./overview.scss";
 import Image from "next/image";
-import CircularProgress from "../CircularProgress/CircularProgress";
+import CircularProgress from "@/components/CircularProgress/CircularProgress";
 import HorizentalProgress from "@/components/HorizentalProgress/HorizentalProgress";
 
 const Overview = () => {
