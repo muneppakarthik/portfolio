2f8d621b563db90baaed974739359d772dd648b2
M Karthik <139629620+muneppakarthik@users.noreply.github.com>
2025-10-01 12:20:19 +0530

    menu design

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,21 +1,58 @@
+"use client";
+
 import React from "react";
 import "./Menu.scss";
+import MenuList from "./MenuList";
+import Model from "../Model/Model";
+import { useGlobal } from "@/context/GlobalContext";
 
 const Menu = () => {
+  const { openMenu, setOpenMenu } = useGlobal();
+  const mainMenuData = [
+    {
+      label: "home",
+      to: "",
+    },
+    {
+      label: "about",
+      to: "",
+    },
+    {
+      label: "contact",
+      to: "",
+    },
+  ];
+  const modelMenuData = [
+    {
+      label: "home",
+      to: "",
+    },
+    {
+      label: "about",
+      to: "",
+    },
+    {
+      label: "contact",
+      to: "",
+    },
+  ];
   return (
     <div className="menu-container">
       <div className="menu-container__menu-icon">
-        <button type="button">
+        <button type="button" onClick={() => setOpenMenu(true)}>
           <i className="icon fa-solid fa-bars"></i>
         </button>
       </div>
-      <div className="menu-container__main-list">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </div>
+      <MenuList data={mainMenuData} />
+      {openMenu && (
+        <Model>
+          <MenuList
+            data={modelMenuData}
+            isModel={true}
+            onClose={() => setOpenMenu(false)}
+          />
+        </Model>
+      )}
     </div>
   );
 };
